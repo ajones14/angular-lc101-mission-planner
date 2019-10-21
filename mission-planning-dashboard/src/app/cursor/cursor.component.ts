11be5170db5ef9e8f280fa6ee1c108735e61cd8c
Abigail Wiggins <rebtaylor14@gmail.com>
2019-10-21 15:45:30 -0500

    Bonus missions complete

---
+++ mission-planning-dashboard/src/app/cursor/cursor.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursor',
+  templateUrl: './cursor.component.html',
+  styleUrls: ['./cursor.component.css']
+})
+export class CursorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
