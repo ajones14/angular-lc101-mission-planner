fae317fd6fbae498568268f527e6a0cf83ae395f
Abigail Wiggins <rebtaylor14@gmail.com>
2019-10-29 00:39:19 -0500

    Studio Completed

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -26,5 +26,11 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(candidate: object) {
+    if (this.crew.includes(candidate)) {
+      this.crew.splice(this.crew.indexOf(candidate), 1);
+    } else if (this.crew.length < 3 && !this.crew.includes(candidate)) {
+      this.crew.push(candidate);
+    }
+  }
 }
